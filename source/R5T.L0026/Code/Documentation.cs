80fc89930835d5ef1f8a96b86c01f32e7dca3151
davidcoats <david@getsafetycone.com>
2022-12-08 12:20:31 -0800

    Initial commit.

---
+++ source/R5T.L0026/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.L0026
+{
+	/// <summary>
+	/// Instance type data library.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.L0026/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.L0026
+Instance type data library.
